dad7a6a88e86d20f5d8c0e9725e9f0d90714852c
root <root@bogon.intra.legendsec.com>
2017-11-15 06:34:06 -0500

    a

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cp .vimrc ~/.vimrc
-if [ -f "~/.vim/bundle/Vundle.vim" ]; then
+if test -e ~/.vim/bundle/Vundle.vim 
+then
+    echo "vundle exist"
+else
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
